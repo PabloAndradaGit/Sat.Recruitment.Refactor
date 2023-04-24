0a925ad19daec3b202a3c44409007a695722c4dd
PabloAndradaGit <andrada.pablo07@gmail.com>
2023-04-24 18:07:03 -0300

    add refactor local, add handlers, EF, Arch, Exception, Configures, UnitTest

---
+++ Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sat.Recruitment.Business.User.CreateUser;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Api.Controllers
+{
+    public class Result
+    {
+        public bool IsSuccess { get; set; }
+        public string Errors { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public partial class UsersController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("/create-user")]
+        public async Task<CreateUserResult> CreateUser(CreateUserCommand createUserCommand)
+        {
+            return await mediator.Send(createUserCommand);
+        }
+    }
+
+}
